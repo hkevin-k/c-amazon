41b65efb05e3891d4e4a26928cb6d9fcf898fea3
hKevin <1946860243@qq.com>
2020-05-29 08:45:30 +0800

    sale_order/get_by_order_ids 接口增加店铺参数

--- src/request/SaleOrderGetByOrderIds.php
+++ src/request/SaleOrderGetByOrderIds.php
@@ -8,6 +8,8 @@ class SaleOrderGetByOrderIds extends Base
 
     private $_order_ids;
 
+    private $_market_ids;
+
     /**
      * @param $order_ids 数组的json格式
      */
@@ -15,5 +17,17 @@ class SaleOrderGetByOrderIds extends Base
     {
         $this->_order_ids = $order_ids;
         $this->apiParas['order_ids'] = $order_ids;
+    }
+
+    /**
+     * 可选
+     * @param $market_ids 数组的json格式
+     */
+    public function setMarketIds($market_ids)
+    {
+        if (!empty($market_ids)) {
+            $this->_market_ids = $market_ids;
+            $this->apiParas['market_ids'] = $market_ids;
+        }
     }
 }
