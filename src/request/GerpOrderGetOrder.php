d12d0f0bb58a66171511389e1725c7f3f19ae545
hkevin <1946860243@qq.com>
2022-01-03 15:17:09 +0800

    增加gerp接口查询亚马逊订单信息

---
+++ src/request/GerpOrderGetOrder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace camazon\request;
+
+/**
+ * 请求gerp接口查询订单信息
+ * @package camazon\request
+ */
+class GerpOrderGetOrder extends Base
+{
+    protected $uri = '/api/v1/gerp-order/get-order';
+
+    private $_order_id;
+
+    private $_seller_order_id;
+
+    /**
+     * @param string
+     */
+    public function setOrderId($order_id)
+    {
+        $this->_order_id = $order_id;
+        $this->apiParas['order_id'] = $order_id;
+    }
+
+    /**
+     * @param string
+     */
+    public function setSellerOrderId($seller_order_id)
+    {
+        if (!empty($market_ids)) {
+            $this->_seller_order_id = $seller_order_id;
+            $this->apiParas['seller_order_id'] = $seller_order_id;
+        }
+    }
+}
